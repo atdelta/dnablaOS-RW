de223795fe1b8132a452df7a85f55eabd9dbc4e2
litecave <64094645+litecave@users.noreply.github.com>
2022-02-17 23:12:23 +0300

    EXPERIMENTAL 1 - Testing using libc.h

--- kernel.c
+++ kernel.c
@@ -10,30 +10,7 @@
 
 void kmain(void)
 {
-	const char *str = "dnablaOS - Started";
-	char *vidptr = (char*)0xb8000; 	//VidMemory starts here
-	unsigned int i = 0;
-	unsigned int j = 0;
-
-	/* this cycle clears screen */
-	while(j < 80 * 25 * 2) {
-		/* пустой символ */
-		vidptr[j] = ' ';
-		/* байт атрибутов */
-		vidptr[j+1] = 0x07; 		
-		j = j + 2;
-	}
-
-	j = 0;
-
-	/* in this cycle string writing into VidMemory */
-	while(str[j] != '\0') {
-		/* ascii displaying */
-		vidptr[i] = str[j];
-		vidptr[i+1] = 0x07;
-		++j;
-		i = i + 2;
-	}
-
+	clearscreen(); // clear screen cycle
+	writeline("experiment1"); // writing line "experiment1"
 	return;
 }
